3e545a644050e75aa94dd12a2d0bd75b3dc508ae
Adam Naoui <adam.naoui@polymtl.ca>
2022-10-26 12:40:18 -0400

    Vector-icons now works

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,33 @@
 import React, {useContext} from "react";
-import {Button, Text, View} from "react-native";
+import {Text} from "react-native";
 import {ClientAuthenticationContext} from "../../context/ClientAuthenticationContext";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import {BottomNavigation} from "react-native-paper";
+
+const MusicRoute = () => <Text>Music</Text>;
+
+const AlbumsRoute = () => <Text>Albums</Text>;
+
+const RecentsRoute = () => <Text>Recents</Text>;
+
+const NotificationsRoute = () => <Text>Notifications</Text>;
 
 export const Home = () => {
   const {setClientId} = useContext(ClientAuthenticationContext);
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'music', title: 'Favorites', focusedIcon: 'heart', unfocusedIcon: 'heart-outline'},
+    { key: 'albums', title: 'Albums', focusedIcon: 'album' },
+    { key: 'recents', title: 'Recents', focusedIcon: 'history' },
+    { key: 'notifications', title: 'Notifications', focusedIcon: 'bell', unfocusedIcon: 'bell-outline' },
+  ]);
+  const renderScene = BottomNavigation.SceneMap({
+    music: MusicRoute,
+    albums: AlbumsRoute,
+    recents: RecentsRoute,
+    notifications: NotificationsRoute,
+  });
+
 
   const handleLogout = async () => {
       setClientId('');
@@ -13,9 +36,10 @@ export const Home = () => {
     );
   }
   return (
-    <View style={{flex: 1, flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
-      <Text>Home</Text>
-      <Button title={"LOG OUT"} onPress={handleLogout}/>
-    </View>
+    <BottomNavigation
+      navigationState={{ index, routes }}
+      onIndexChange={setIndex}
+      renderScene={renderScene}
+    />
   )
 }

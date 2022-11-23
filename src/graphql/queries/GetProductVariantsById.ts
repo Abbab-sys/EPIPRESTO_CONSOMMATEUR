130ba57ddba457a37816aa1f0ab89ea0e19f766d
Adam Naoui <adam.naoui@polymtl.ca>
2022-11-23 11:12:58 -0500

    hotfix: Product query

--- src/graphql/queries/GetProductVariantsById.ts
+++ src/graphql/queries/GetProductVariantsById.ts
@@ -2,37 +2,37 @@ import {gql} from "@apollo/client";
 
 
 export const GET_PRODUCT_VARIANTS_BY_ID = gql`
-    query Query($idProduct: ID!) {
+    query Query($idProduct: ID!, $first: Int, $offset: Int!) {
         getProductById(idProduct: $idProduct) {
-        code
-        message
-        product {
-            _id
-            brand
-            description
-            imgSrc
-            title
-            relatedStore {
+            code
+            message
+            product {
                 _id
-                name
-              }
-            variants {
-            _id
-            displayName
-            imgSrc
-            stock
-            price
-            byWeight
-            availableForSale
-            taxable
-            relatedProduct {
                 brand
-                title
-                tags
                 description
-              }
+                imgSrc
+                title
+                relatedStore {
+                    _id
+                    name
+                }
+                variants(first: $first, offset: $offset) {
+                    _id
+                    displayName
+                    imgSrc
+                    stock
+                    price
+                    byWeight
+                    availableForSale
+                    taxable
+                    relatedProduct {
+                        brand
+                        title
+                        tags
+                        description
+                    }
+                }
             }
-        }
         }
     }
 `
--- src/pages/product/ProductPage.tsx
+++ src/pages/product/ProductPage.tsx
@@ -1,147 +1,147 @@
-import { useQuery } from "@apollo/client";
-import React, { useEffect, useState } from "react";
-import { ActivityIndicator, FlatList, SafeAreaView, View } from "react-native";
-import { Button, Searchbar, Snackbar, Text } from 'react-native-paper';
-import { Float } from "react-native/Libraries/Types/CodegenTypes";
-import { GET_PRODUCT_VARIANTS_BY_ID } from "../../graphql/queries/GetProductVariantsById";
-import Product, { VariantProps } from "../stores/subsections/Product";
-import { productPageStyles } from "./ProductPageStyles";
-import { useCartManager } from "../../hooks/management/useCartManager";
-import { useTranslation } from "react-i18next";
+import {useQuery} from "@apollo/client";
+import React, {useEffect, useState} from "react";
+import {ActivityIndicator, FlatList, SafeAreaView, View} from "react-native";
+import {Button, Text} from 'react-native-paper';
+import {Float} from "react-native/Libraries/Types/CodegenTypes";
+import {GET_PRODUCT_VARIANTS_BY_ID} from "../../graphql/queries/GetProductVariantsById";
+import Product, {VariantProps} from "../stores/subsections/Product";
+import {productPageStyles} from "./ProductPageStyles";
+import {useCartManager} from "../../hooks/management/useCartManager";
+import {useTranslation} from "react-i18next";
 
-const ProductPage = ({ idProduct, goBack, route }: any) => {
-    const { t } = useTranslation('translation')
+const ProductPage = ({idProduct, goBack, route}: any) => {
+  const {t} = useTranslation('translation')
 
-    let finalGoBack = goBack;
-    if (route?.params?.goBack) {
-        finalGoBack = route.params.goBack;
+  let finalGoBack = goBack;
+  if (route?.params?.goBack) {
+    finalGoBack = route.params.goBack;
+  }
+  let finalIdProduct = idProduct;
+  if (route?.params?.idProduct) {
+    finalIdProduct = route.params.idProduct;
+  }
+
+  const [visible, setVisible] = React.useState(false);
+
+  const onToggleSnackBar = () => setVisible(true);
+
+  const onDismissSnackBar = () => setVisible(false);
+
+
+  const [variants, setVariants] = useState<VariantProps[]>([])
+
+  const {data, loading, error, fetchMore} = useQuery(GET_PRODUCT_VARIANTS_BY_ID, {
+    variables: {
+      idProduct: finalIdProduct,
+      offset: 0
+    },
+    fetchPolicy: "network-only",
+    onCompleted(data) {
+      const product = data.getProductById.product
+      const publishedVariants = product.variants.filter((variant: any) => {
+        return variant.availableForSale
+      })
+      setVariants(publishedVariants)
+
+    },
+
+  })
+
+  // close snackbar after 3 seconds
+  useEffect(() => {
+    if (visible) {
+      const timeout = setTimeout(() => onDismissSnackBar(), 3000);
+      return () => {
+        clearTimeout(timeout);
+      };
     }
-    let finalIdProduct = idProduct;
-    if (route?.params?.idProduct) {
-        finalIdProduct = route.params.idProduct;
-    }
+  }, [visible]);
 
-    const [visible, setVisible] = React.useState(false);
-
-    const onToggleSnackBar = () => setVisible(true);
-
-    const onDismissSnackBar = () => setVisible(false);
+  const {addVariantToCart} = useCartManager();
 
 
-    const [variants, setVariants] = useState<VariantProps[]>([])
-
-    const { data, loading, error, fetchMore } = useQuery(GET_PRODUCT_VARIANTS_BY_ID, {
-        variables: {
-            idProduct: finalIdProduct
-        },
-        fetchPolicy: "network-only",
-        onCompleted(data) {
-            const product = data.getProductById.product
-            const publishedVariants = product.variants.filter((variant: any) => {
-                return variant.availableForSale
-            })
-            setVariants(publishedVariants)
-
-        },
-
-    })
-
-    // close snackbar after 3 seconds
-    useEffect(() => {
-        if (visible) {
-            const timeout = setTimeout(() => onDismissSnackBar(), 3000);
-            return () => {
-                clearTimeout(timeout);
-            };
-        }
-    }, [visible]);
-
-    const { addVariantToCart } = useCartManager();
-
-
-    return (
-        <SafeAreaView>
-            <View style={productPageStyles.view}>
-                <Button icon="arrow-left-circle" mode="contained" onPress={finalGoBack}>
-                </Button>
-                <Text variant="headlineMedium" style={productPageStyles.headline}>
-                    {data ? data.getProductById.product.title : "Product"}
-                </Text>
-                <Text variant="labelLarge">
-                    {data ? data.getProductById.product.relatedStore.name : "Name"}
-                </Text>
+  return (
+    <SafeAreaView>
+      <View style={productPageStyles.view}>
+        <Button icon="arrow-left-circle" mode="contained" onPress={finalGoBack}>
+        </Button>
+        <Text variant="headlineMedium" style={productPageStyles.headline}>
+          {data ? data.getProductById.product.title : "Product"}
+        </Text>
+        <Text variant="labelLarge">
+          {data ? data.getProductById.product.relatedStore.name : "Name"}
+        </Text>
+      </View>
+      <SafeAreaView>
+        {
+          loading ? (
+            <View style={productPageStyles.innerContainer}>
+              <ActivityIndicator size="large" color="#FFA500"></ActivityIndicator>
             </View>
-            <SafeAreaView>
-                {
-                    loading ? (
-                        <View style={productPageStyles.innerContainer}>
-                            <ActivityIndicator size="large" color="#FFA500"></ActivityIndicator>
-                        </View>
-                    ) : error ? (
-                        <View style={productPageStyles.innerContainer}>
-                            <Text style={productPageStyles.errorText}>{t('store.data.error')}</Text>
-                        </View>)
-                        : (
-                            variants.length === 0 ?
+          ) : error ? (
+              <View style={productPageStyles.innerContainer}>
+                <Text style={productPageStyles.errorText}>{t('store.data.error')}</Text>
+              </View>)
+            : (
+              variants.length === 0 ?
 
-                                (<Text>{t('store.data.noProducts')}</Text>)
-                                :
-                                (
-                                    <FlatList
-                                        numColumns={2}
-                                        data={variants}
-                                        renderItem={({ item }) =>
-                                            <Product
-                                                _id={item._id}
-                                                displayName={item.displayName}
-                                                imgSrc={item.imgSrc ? item.imgSrc : "https://img.icons8.com/ios/452/no-image.png"}
-                                                stock={item.stock}
-                                                price={item.price}
-                                                byWeight={item.byWeight}
-                                                taxable={item.taxable}
-                                                relatedProduct={item.relatedProduct}
-                                                availableForSale={item.availableForSale}
-                                                addToCart={(quantity: Float) => {
-                                                    addVariantToCart({
-                                                        variantId: item._id,
-                                                        variantName: item.displayName,
-                                                        storeId: data.getProductById.product.relatedStore._id,
-                                                        storeName: data.getProductById.product.relatedStore.name,
-                                                        price: item.price,
-                                                        imageSrc: item.imgSrc ? item.imgSrc : "https://img.icons8.com/ios/452/no-image.png",
-                                                        taxable: item.taxable,
+                (<Text>{t('store.data.noProducts')}</Text>)
+                :
+                (
+                  <FlatList
+                    numColumns={2}
+                    data={variants}
+                    renderItem={({item}) =>
+                      <Product
+                        _id={item._id}
+                        displayName={item.displayName}
+                        imgSrc={item.imgSrc ? item.imgSrc : "https://img.icons8.com/ios/452/no-image.png"}
+                        stock={item.stock}
+                        price={item.price}
+                        byWeight={item.byWeight}
+                        taxable={item.taxable}
+                        relatedProduct={item.relatedProduct}
+                        availableForSale={item.availableForSale}
+                        addToCart={(quantity: Float) => {
+                          addVariantToCart({
+                            variantId: item._id,
+                            variantName: item.displayName,
+                            storeId: data.getProductById.product.relatedStore._id,
+                            storeName: data.getProductById.product.relatedStore.name,
+                            price: item.price,
+                            imageSrc: item.imgSrc ? item.imgSrc : "https://img.icons8.com/ios/452/no-image.png",
+                            taxable: item.taxable,
 
-                                                    }, quantity)
-                                                    onToggleSnackBar()
-                                                }}
-                                            />
-                                        }
-                                        keyExtractor={item => item._id}
-                                        onEndReachedThreshold={1}
-                                        onEndReached={() => {
-                                            if (data.getProductById.product.variants.length > variants.length) {
-                                                fetchMore({
-                                                    variables: {
-                                                        offset: variants.length
-                                                    },
-                                                    updateQuery: (prev, { fetchMoreResult }) => {
-                                                        if (!fetchMoreResult) return prev;
-                                                        const remainingVariants = fetchMoreResult.getProductById.product.variants
-                                                        const publishedVariants = remainingVariants.filter((variant: any) => {
-                                                            return variant.availableForSale
-                                                        })
-                                                        setVariants([...variants, ...publishedVariants])
-                                                    }
-                                                })
-                                            }
-                                        }}
-                                    />
-                                )
-                        )}
-            </SafeAreaView>
-        </SafeAreaView>
-    )
-
+                          }, quantity)
+                          onToggleSnackBar()
+                        }}
+                        relatedStoreIsPaused={item.relatedStoreIsPaused}/>
+                    }
+                    keyExtractor={item => item._id}
+                    onEndReachedThreshold={1}
+                    onEndReached={() => {
+                      if (data.getProductById.product.variants.length > variants.length) {
+                        fetchMore({
+                          variables: {
+                            offset: variants.length
+                          },
+                          updateQuery: (prev, {fetchMoreResult}) => {
+                            if (!fetchMoreResult) return prev;
+                            const remainingVariants = fetchMoreResult.getProductById.product.variants
+                            const publishedVariants = remainingVariants.filter((variant: any) => {
+                              return variant.availableForSale
+                            })
+                            setVariants([...variants, ...publishedVariants])
+                          }
+                        })
+                      }
+                    }}
+                  />
+                )
+            )}
+      </SafeAreaView>
+    </SafeAreaView>
+  )
 
 
 }

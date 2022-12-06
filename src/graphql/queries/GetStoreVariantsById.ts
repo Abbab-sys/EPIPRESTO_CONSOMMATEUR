dda7cb0508d4543e3ae2383b54b510fa70befa93
alexv <alessandro.van.reusel@gmail.com>
2022-12-05 21:13:42 -0500

    bug: correcting query for getting variants

--- src/graphql/queries/GetStoreVariantsById.ts
+++ src/graphql/queries/GetStoreVariantsById.ts
@@ -10,7 +10,7 @@ export const GET_STORE_VARIANTS_BY_ID = gql`
   query GetStoreById(
     $idStore: ID!
     $offset: Int!
-    $first: Int
+    $first: Int!
     $searchText: String
   ) {
     getStoreById(idStore: $idStore) {
